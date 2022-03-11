202fa66dbffc2d6e41b9a555db0a71e704073678
Paulo Peres <88409939+gugas1250@users.noreply.github.com>
2022-03-11 15:12:28 -0800

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,2 +1,1 @@
 aws ec2 run-instances --image-id ami-0359b3157f016ae46 --count 1 --instance-type t2.micro --key-name new_key --region us-west-2
-#aws ec2 run-instances --image-id ami-0359b3157f016ae46 --count 1 --instance-type t2.micro --key-name new_key --security-groups my-sg
