d9fe294feceb30ec17266dbe084b58b7fb378a9c
Paulo Peres <88409939+gugas1250@users.noreply.github.com>
2022-03-11 16:59:43 -0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,24 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=3.70.0,<=3.73.0"
+      version = ">=3.34.0,<=3.35.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
-  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
+  region     = "us-west-2"
+  #shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
   profile = "default"
-
 }
 
-
 resource "aws_instance" "my-first-ec2" {
-  ami           = "ami-0359b3157f016ae46"
+  ami           = "ami-013a129d325529d4d"
   instance_type = "t2.micro"
   tags = {
     Name  = "myec2-1"
-    Owner = "Paulo"
+    Owner = "paulo"
   }
 }
